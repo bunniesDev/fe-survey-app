c16f805a72912c7f804e46023f3652c4f5873cb6
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-03 02:54:35 +0900

    :memo: 머지업스트림 충돌 수정중

--- src/App.js
+++ src/App.js
@@ -1,143 +1,197 @@
-import styled from 'styled-components';
-import React, { useState } from 'react';
-import { MdKeyboardArrowLeft } from 'react-icons/md';
-import MainLayout from './components/UI/layouts/MainLayout';
-import Card from './components/UI/Card';
-import ProgressBar from './components/UI/ProgressBar';
-import RadioButtonGroup from './components/UI/RadioButton/RadioButtonGroup';
-import RadioButton from './components/UI/RadioButton/RadioButton';
+import { useState } from 'react';
+import { MdCheck, MdBarChart, MdKeyboardArrowLeft } from 'react-icons/md';
+import { FaAirbnb, FaRegGrinTongueSquint } from 'react-icons/fa';
 import Button from './components/UI/Button';
-import data from './data';
-import submitData from './submit';
+import Card from './components/UI/Card';
+import Colmn from './components/UI/Grid/Column';
+import Row from './components/UI/Grid/Row';
+import RadioButton from './components/UI/RadioButton/RadioButton';
+import RadioButtonGroup from './components/UI/RadioButton/RadioButtonGroup';
+import GlobalStyles from './GlobalStyles';
+import MainLayout from './components/UI/layouts/MainLayout';
+import useDialogs from './components/UI/Dialog/useDialogs';
+import ProgressBar from './components/UI/ProgressBar';
+import DialogAlerts from './components/UI/Dialog/DialogAlerts';
 
-const Count = styled.h4`
-  text-align: center;
-  margin-top: 40px;
-  margin-bottom: 60px;
-  font-size: 20px;
-`;
+const OPTIONS = [
+  { id: 1, text: '리액트' },
+  { id: 2, text: '뷰' },
+  { id: 3, text: '앵귤러' },
+  { id: 4, text: '스벨트' },
+  { id: 5, text: '기타' },
+];
 
-const Question = styled.h2`
-  text-align: center;
-  margin-top: 40px;
-  margin-bottom: 60px;
-`;
-const Blank = styled.div`
-  margin-bottom: ${props => props.marginBottom}px;
-`;
-const NotSeletedMessage = styled.div`
-  color: red;
-  display: flex;
-  justify-content: center;
-`;
-const NotSeletedMessageFont = styled.h3`
-  margin: 5px;
-`;
+function MyModal({ onClose, onSubmit }) {
+  return (
+    <DialogAlerts
+      onSubmit={onSubmit}
+      onClose={onClose}
+      title="타이틀"
+      labelClose="돌아가기"
+      labelSubmit="제출하기"
+    >
+      모달입니다
+    </DialogAlerts>
+  );
+}
 
-let count = 0;
+function App() {
+  const [selectedVaule, setSelectedValue] = useState(null);
+  const { openDialog } = useDialogs();
 
-export default function App1() {
-  const [selectedVaule, setSelectedValue] = useState(submitData);
-  const [question, setQuestion] = useState(data[count].question);
-  const [counter, setCounter] = useState(data[count].id + 1);
-  const [answer, setAnswer] = useState(data[count].answer);
-  const [notSeleted, setNotSeleted] = useState(false);
+  const handleClickStart = () => {
+    // console.log('시작하기');
+  };
 
-  // [{id:0 select:v},]
   const handleChangeValue = v => {
-    if (count === selectedVaule[count].id) {
-      const update = [...selectedVaule];
-      update.splice(count, 1, { id: count, select: v });
-      console.log(update);
-      setSelectedValue(update);
-    }
-  };
-  const submitHandler = () => {
-    if (selectedVaule[count].select === null) {
-      setNotSeleted(true);
-      return;
-    }
-    setNotSeleted(false);
-  };
-  const nextQuestionHandler = () => {
-    if (selectedVaule[count].select === null) {
-      setNotSeleted(true);
-      return;
-    }
-    count += 1;
-    setQuestion(data[count].question);
-    setCounter(data[count].id + 1);
-    setAnswer(data[count].answer);
-    setNotSeleted(false);
+    setSelectedValue(v);
   };
 
-  const prevQuestionHandler = () => {
-    count -= 1;
-    setQuestion(data[count].question);
-    setCounter(data[count].id + 1);
-    setAnswer(data[count].answer);
-    setNotSeleted(false);
+  const handleClickOpenDialog = () => {
+    openDialog(MyModal, {
+      onSubmit: () => {
+        // console.log('비지니스 로직 처리...');
+      },
+    });
   };
-
   return (
     <MainLayout>
       <Card>
-        {count === 0 ? null : (
-          <Button
-            variant="text"
-            size="sm"
-            startIcon={<MdKeyboardArrowLeft />}
-            onClick={prevQuestionHandler}
-          >
-            이전질문
-          </Button>
-        )}
-        <Count>
-          {counter} of {data.length}
-        </Count>
-        <ProgressBar step={counter} total="20" />
-        <Question>Q. {question}</Question>
+        <ProgressBar step="4" total="20" />
+      </Card>
+      <Card>
         <RadioButtonGroup
           label="질문 1"
-          value={selectedVaule[count].select}
+          value={selectedVaule}
           name="question-1"
           onChange={handleChangeValue}
         >
-          {answer.map((option, index) => (
+          {OPTIONS.map(option => (
             <RadioButton
-              id={`radio-${data[index].id}`}
+              id={`radio-${option.id}`}
               block
-              key={data[index].id}
-              value={option}
+              key={option.id}
+              value={option.text}
             >
-              {option}
+              {option.text}
             </RadioButton>
           ))}
         </RadioButtonGroup>
-        <Blank marginBottom={40} />
-
-        {count === data.length - 1 ? (
-          <Button variant="primary" size="lg" block onClick={submitHandler}>
-            제출하기
-          </Button>
-        ) : (
-          <Button
-            variant="primary"
-            size="lg"
-            block
-            onClick={nextQuestionHandler}
-          >
-            다음
-          </Button>
-        )}
-        {notSeleted ? (
-          <NotSeletedMessage>
-            <NotSeletedMessageFont>답변을 선택해주세요</NotSeletedMessageFont>
-          </NotSeletedMessage>
-        ) : (
-          <Blank marginBottom={50} />
-        )}
+        <p>선택된 값 : {selectedVaule}</p>
       </Card>
+      <Card>
+        <Row gutter="md">
+          <Colmn cols="6" md="4" lg="2">
+            <Button
+              variant="secondary"
+              size="lg"
+              block
+              onClick={handleClickStart}
+            >
+              돌아가기
+            </Button>
+          </Colmn>
+          <Colmn cols="6" md="8" lg="10">
+            <Button
+              variant="primary"
+              size="lg"
+              block
+              startIcon={<MdBarChart />}
+            >
+              결과보기
+            </Button>
+          </Colmn>
+        </Row>
+      </Card>
+      <Card>
+        <Button variant="primary" size="lg" block>
+          시작하기
+        </Button>
+        <Button variant="secondary" size="lg" block>
+          결과보기
+        </Button>
+      </Card>
+      <Card>
+        <Row>
+          <Button variant="primary" size="lg" startIcon={<MdCheck />}>
+            아이콘
+          </Button>
+          <Button variant="secondary" size="lg" endIcon={<MdCheck />}>
+            아이콘
+          </Button>
+          <Button variant="danger" size="lg" startIcon={<FaAirbnb />}>
+            아이콘
+          </Button>
+          <Button
+            variant="danger"
+            size="lg"
+            disabled
+            endIcon={<FaRegGrinTongueSquint />}
+          >
+            아이콘
+          </Button>
+        </Row>
+        <Row>
+          <Button variant="text" size="lg" startIcon={<MdKeyboardArrowLeft />}>
+            뒤로가기
+          </Button>
+          <Button variant="text" startIcon={<MdKeyboardArrowLeft />}>
+            뒤로가기
+          </Button>
+          <Button variant="text" size="sm" startIcon={<MdKeyboardArrowLeft />}>
+            뒤로가기
+          </Button>
+        </Row>
+      </Card>
+      <Card>
+        <Row>
+          <Button variant="primary" size="lg">
+            Primary
+          </Button>
+          <Button variant="secondary" size="lg">
+            Secondary
+          </Button>
+          <Button variant="danger" size="lg">
+            Error
+          </Button>
+          <Button disabled="disabled" size="lg">
+            Disabled
+          </Button>
+          <Button variant="text" size="lg">
+            TEXT
+          </Button>
+        </Row>
+        <Row>
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="danger">Error</Button>
+          <Button disabled="disabled">Disabled</Button>
+          <Button variant="text">TEXT</Button>
+        </Row>
+        <Row>
+          <Button variant="primary" size="sm">
+            Primary
+          </Button>
+          <Button variant="secondary" size="sm">
+            Secondary
+          </Button>
+          <Button variant="danger" size="sm">
+            Error
+          </Button>
+          <Button disabled="disabled" size="sm">
+            Disabled
+          </Button>
+          <Button variant="text" size="sm">
+            TEXT
+          </Button>
+        </Row>
+      </Card>
+      <Card>
+        <Button onClick={handleClickOpenDialog}>모달</Button>
+      </Card>
+      <GlobalStyles />
     </MainLayout>
   );
 }
+
+export default App;
